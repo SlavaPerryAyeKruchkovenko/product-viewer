84c605e31db19c46a4d38f4c7c909538d2fa21ce
Slava <kruchkovenko@gmail.com>
2023-03-26 14:03:11 +0500

    add change color

--- app/src/main/java/com/example/product_viewer/SecondFragment.kt
+++ app/src/main/java/com/example/product_viewer/SecondFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.example.product_viewer.databinding.FragmentSecondBinding
 
@@ -40,5 +42,12 @@ class SecondFragment : Fragment() {
     }
     private fun init(){
         binding.productToolbar.setNavigationOnClickListener { findNavController().popBackStack() }
+        binding.switchColor.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.textLayout.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.purple_200))
+            } else {
+                binding.textLayout.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.purple_300))
+            }
+        })
     }
 }
